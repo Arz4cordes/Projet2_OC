c14f1e48c947ab3cfdf069494eaa496da471fd32
Armel <73070831+Arz4cordes@users.noreply.github.com>
2020-11-11 20:34:54 +0100

    Add files via upload

---
+++ books_price_extract.py
@@ -0,0 +1,18 @@
+#Programme principal:
+
+# aller sur la page d'accueil du site
+# récupérer les différentes catégories dans un menu du site
+# écrire ces catégories dans une liste category
+# lancer une boucle pour parcourir les catégories:
+    
+    # lancer le module books_category
+        # le module books_category va sur la page d'une catégorie donnée,
+        # puis crée une liste books qui contient les livres de la catégorie,
+        # crée le fichier csv de la catégorie,
+        # puis lance une boucle pour parcourir ces livres:
+            # récupèrer les infos du livre en lançant le module books_information:
+                # le module books_informations va sur la page d'un livre,
+                # puis récupère les infos sur ce livre
+                # elle met les infos dans un tableau booksInformations
+            # écrire les infos du livre dans une nouvelle ligne du fichier csv de la catégorie
+        
